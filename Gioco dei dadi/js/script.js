a3ec13d3a86f5afe8c7453861c618a0822274a70
Yomakota <teoperitore@outlook.com>
2021-11-24 17:37:50 +0100

    add-var

---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,10 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+let randomNumberUser = Math.floor(Math.random() * 6) + 1;
+let randomNumberPc = Math.floor(Math.random() * 6) + 1;
+console.log(numberUser, numberPc);
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
