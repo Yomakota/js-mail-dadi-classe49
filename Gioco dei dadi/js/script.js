d3f3536d2f7a3a7589bb392b6c18deb5db8aa8d9
Yomakota <teoperitore@outlook.com>
2021-11-24 19:38:31 +0100

    ended

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -3,19 +3,23 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-let randomNumberUser = Math.floor(Math.random() * 6) + 1;
-let randomNumberPc = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumberUser, randomNumberPc);
+let scoreUser = Math.floor(Math.random() * 6) + 1;
+let scorePc = Math.floor(Math.random() * 6) + 1;
+console.log(scoreUser, scorePc);
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 let winner = document.getElementById("winner");
 
-if (randomNumberUser > randomNumberPc) {
+if (scoreUser > scorePc) {
     winner.innerHTML = "Player Winner";
+    document.getElementById("score-player").innerHTML = scoreUser;
+    document.getElementById("score-CPU").innerHTML = scorePc;
 } 
 
-else if (randomNumberUser < randomNumberPc) {
+else if (scoreUser < scorePc) {
     winner.innerHTML = "CPU Winner";
+    document.getElementById("score-CPU").innerHTML = scoreUser;
+    document.getElementById("score-player").innerHTML = scorePc;
 } 
 
 else {
