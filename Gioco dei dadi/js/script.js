1e1d7d8be36b45697d478c5521fed8ef82373f98
Yomakota <teoperitore@outlook.com>
2021-11-24 17:51:56 +0100

    add-if

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -5,6 +5,17 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 let randomNumberUser = Math.floor(Math.random() * 6) + 1;
 let randomNumberPc = Math.floor(Math.random() * 6) + 1;
-console.log(numberUser, numberPc);
+console.log(randomNumberUser, randomNumberPc    );
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+if (randomNumberUser > randomNumberPc) {
+    
+} 
+
+else if (randomNumberUser < randomNumberPc) {
+    
+} 
+
+else {
+    
+}
