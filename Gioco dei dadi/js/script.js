b82a42fd3541c3de2fe2f075fa6a02538109bf12
Yomakota <teoperitore@outlook.com>
2021-11-24 18:17:43 +0100

    add-winner

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -5,17 +5,19 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 let randomNumberUser = Math.floor(Math.random() * 6) + 1;
 let randomNumberPc = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumberUser, randomNumberPc    );
+console.log(randomNumberUser, randomNumberPc);
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+let winner = document.getElementById("winner");
+
 if (randomNumberUser > randomNumberPc) {
-    
+    winner.innerHTML = "Player Winner";
 } 
 
 else if (randomNumberUser < randomNumberPc) {
-    
+    winner.innerHTML = "CPU Winner";
 } 
 
 else {
-    
+    winner.innerHTML = "Draw";
 }
